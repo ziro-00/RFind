d37cb9717c4d25b82473a85073d3d90fc4c0e0ee
ziro-00 <90027705+ziro-00@users.noreply.github.com>
2023-01-17 14:40:34 +0000

    Update rfind.py

--- rfind.py
+++ rfind.py
@@ -1,1 +1,14 @@
-import request
+import requests
+
+#examples of pages to test from common.txt (/usr/share/seclist/discovery/web-web-content)
+list = open("common.txt", "r")  #"r" <- modo leitura
+
+
+#loop that walks trough the array and shows the stat code and the page name that is currently being tested
+for item in list:
+    url = "http://websitename.com" + item.replace("\n","") # .replace("\n","") <-- serve pra dar replace no "enter" do ficheiro common.txt
+    resposta = requests.get(url)
+    if resposta.status_code == 200:
+        print("Found: " + url)
+    else:
+        print("Not Found: " + url + " (" + str(resposta.status_code) + ")")
