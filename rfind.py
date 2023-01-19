0066c1e0b877994c8044207dffb2c8ca5c046794
ziro-00 <90027705+ziro-00@users.noreply.github.com>
2023-01-19 00:46:37 +0000

    Revert "Readme"

---
+++ rfind.py
@@ -0,0 +1,33 @@
+import requests
+
+list = open("list.txt", "r")
+
+print('''
+
+Built by:
+
+███████╗██╗██████╗  ██████╗        ██████╗  ██████╗ 
+╚══███╔╝██║██╔══██╗██╔═══██╗      ██╔═████╗██╔═████╗
+  ███╔╝ ██║██████╔╝██║   ██║█████╗██║██╔██║██║██╔██║
+ ███╔╝  ██║██╔══██╗██║   ██║╚════╝████╔╝██║████╔╝██║
+███████╗██║██║  ██║╚██████╔╝      ╚██████╔╝╚██████╔╝
+╚══════╝╚═╝╚═╝  ╚═╝ ╚═════╝        ╚═════╝  ╚═════╝ 
+                                                    
+                 ---- RFind ----
+                    
+    * How to use
+    * Insert url: https://www.example.com/
+
+''')
+
+host = input("URL: ")
+print("")
+print("searching . . .\n")
+
+for item in list:
+    url = host + item.replace("\n","")
+    resposta = requests.get(url)
+    if resposta.status_code == 200:
+        print("Found: " + url)
+    #else:
+        #print("Not Found: " + url + " (" + str(resposta.status_code) + ")")
